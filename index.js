7d1c83245e68fadcdfa4322612b565081b0286e8
Hankyol Cho <hancho.career@gmail.com>
2018-07-09 18:16:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+
+
+
+function takeANumber(line){
+  
+}
+
+// takeANumber(line) => "Welcome! You're ticket number #1" [1]
+// takeANumber(line) => "Welcome! You're ticket number #2" [1, 2]
+
+// nowServing(line) => "Currently serving: #1" [2]
+// nowServing(line) => "Currently serving: #2" []
+
+// takeANumber(line) => "Welcome! You're ticket number #3" [3]
+
+
 function takeANumber(line,nPerson)
 {
   line.push(nPerson);
